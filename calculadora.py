b9a0273fb3f3a1fef6f3a4388182a4cc0ebb1ad0
dorgasxds <douglaseduardo.sb@gmail.com>
2025-09-13 11:20:41 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,5 @@
+def calcular_tmb_homem(peso, altura, idade):
+    return 10 * peso + 6.25 * altura - 5 * idade + 5
+
+def calcular_tmb_mulher(peso, altura, idade):
+    return 10 * peso + 6.25 * altura - 5 * idade - 161
